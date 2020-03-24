9783d1bd639f91147c62cd828ba13ab317a44640
RyanARamirez <62263509+RyanARamirez@users.noreply.github.com>
2020-03-24 16:08:29 -0500

    Initial commit

---
+++ WacthSTMI Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WacthSTMI Extension
+//
+//  Created by Ryan Ramirez on 3/22/20.
+//  Copyright © 2020 Ryan Ramirez. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ WacthSTMI Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WacthSTMI Extension
+//
+//  Created by Ryan Ramirez on 3/22/20.
+//  Copyright © 2020 Ryan Ramirez. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
